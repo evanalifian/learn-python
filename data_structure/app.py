a1ee7ae51e96a7e6569f1e9aefa78c399309543f
evanalifian <evanalifian@gmal.com>
2025-02-21 02:37:46 +0700

    refactore code and change some variable names

--- data_structure/app.py
+++ data_structure/app.py
@@ -1,10 +1,10 @@
 """
 Import semua module yang dibutuhkan:
-1. Import variables sks_matkul and ket_nilai.
+1. Import variables subjects and grades.
 2. Import utils untuk keperluan kecil.
 """
-from variables import sks_matkul, ket_nilai
-from utils import displayDictKeys, displayMatkulDetails
+from variables import subjects, grades
+from utils import display_subjects, display_user_subjects
 from controllers import calculateIPK
 
 
@@ -16,9 +16,10 @@ Membuat variables and menampilkan daftar matkul:
 2. Membuat variable dict kosong untuk menyimpan daftar matkul yang diisi user.
 3. Menampilkan daftar matkul.
 """
-input_jumlah_matkul = int(input("Masukkan jumlah matkul yang ingin dipilih: "))
-user_matkul = {}
-print(f"\nDaftar matkul:\n{displayDictKeys(sks_matkul)}")
+total_subjects = int(input("Masukkan jumlah matkul yang ingin dipilih: "))
+user_subjects = {}
+display_subjects()
+print()
 
 
 
@@ -30,25 +31,25 @@ Mengisi matkul beserta nilai yang diperoleh dari matkul tersebut:
 3. Melakukan validasi pada input nilai matkul tersedia pada daftar atau tidak.
 4. Menampilkan daftar matkul + nilai yang telah diisi oleh user.
 """
-for i in range(input_jumlah_matkul):
+for i in range(total_subjects):
   while True:
-    input_matkul = input(f"Masukkan nama matkul: ")
-    if input_matkul not in sks_matkul.keys():
+    subject = input(f"Masukkan nama matkul: ")
+    if subject not in subjects.keys():
       print("Matkul yang anda cari tidak ada pada daftar!")
     else:
       break
 
   while True:
-    input_nilai = input(f"Masukkan nilai matkul {input_matkul} (ket: A, B, atau C): ").lower()
-    if input_nilai not in ket_nilai.keys():
+    grade = input(f"Masukkan nilai matkul {subject} (ket: A, B, atau C): ").lower()
+    if grade not in grades.keys():
       print("Nilai yang tersedia: A, B, B+, C, C+, D, D+, dan E")
     else:
       break
 
-  print()
-  user_matkul[input_matkul] = input_nilai
+  user_subjects[subject] = grade
 
-displayMatkulDetails(user_matkul, sks_matkul, ket_nilai)
+print("\nMatkul yang berhasil dipilih:")
+display_user_subjects(user_subjects)
 
 
 
@@ -61,5 +62,5 @@ Menghitung IPK
     - Variable dict daftar nilai
 2. Menampilkan hasil IPK.
 """
-ipk_score = calculateIPK(user_matkul, sks_matkul, ket_nilai)
-print(f"\nNilai IP adalah: {ipk_score}")
+ipk_score = calculateIPK(user_subjects)
+print(f"\nNilai IPs Anda adalah: {ipk_score}")
